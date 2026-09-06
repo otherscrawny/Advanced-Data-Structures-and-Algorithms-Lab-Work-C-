e3de05235ee6bb54a7eaa6b6c39ea6711ffa4de9
otherscrawny <dawanforgames@gmail.com>
2026-09-06 22:51:51 +0530

    minor fix to merge sort

--- set2/mergesort.c
+++ set2/mergesort.c
@@ -6,16 +6,13 @@ void merge(int arr[], int l, int m, int r)
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
     int L[n1], R[n2];
 
-    // Copy data to temp arrays L[] and R[]
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays back into arr[l..r
     i = 0;
     j = 0;
     k = l;
@@ -34,8 +31,6 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 
-    // Copy the remaining elements of L[],
-    // if there are any
     while (i < n1)
     {
         arr[k] = L[i];
@@ -43,8 +38,6 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 
-    // Copy the remaining elements of R[],
-    // if there are any
     while (j < n2)
     {
         arr[k] = R[j];
@@ -53,8 +46,6 @@ void merge(int arr[], int l, int m, int r)
     }
 }
 
-// l is for left index and r is right index of the
-// sub-array of arr to be sorted
 void mergeSort(int arr[], int l, int r)
 {
 
@@ -62,7 +53,6 @@ void mergeSort(int arr[], int l, int r)
     {
         int m = l + (r - l) / 2;
 
-        // Sort first and second halves
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
 
